0285421c623186320bac2feacc49ea74bfc66325
Monica R <mroy.dev@outlook.com>
2022-06-04 16:36:45 -0400

    add twitter functionality

---
+++ quotes.js
@@ -0,0 +1,43 @@
+const localQuotes = [
+
+    {
+        text: 'If there is no struggle, there is no progress.',
+        author: '— Frederick Douglass',
+    },
+
+    {
+        text: 'Remind yourself. Nobody built like you, you design yourself.',
+        author: 'Jay - Z',
+    },
+
+    {
+        text: 'Forget yesterday, live for today. Tomorrow will take care of itself.',
+        author: 'Rick Ross',
+    },
+
+    {
+        text: 'Damn right I like the life I live because I went from negative to positive',
+        author: 'Biggie Smalls',
+    },
+
+    {
+        text: 'Rap is something you do. Hip-hop is something you live.',
+        author: 'KRS-One',
+    },
+
+    {
+        text: 'For every dark night, there’s a brighter day.',
+        author: 'Tupac',
+    },
+
+    {
+        text: 'I just want to be like a good example to somebody in the future.',
+        author: 'Megan Thee Stallion',
+    },
+
+    {
+        text: 'I want to make something commercial that people can pick up on.',
+        author: 'Missy Elliot',
+    }
+
+]
